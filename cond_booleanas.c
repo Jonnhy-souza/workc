43d6e571edd021adb6304f3d25009e4eeeb6860f
Jonnhy Souza <souzajonnhy@gmail.com>
2019-11-05 10:59:31 -0300

    condicionais boleanas

---
+++ cond_booleanas.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main(){
+
+	
+	//Definindo as minhas variaveis	
+	bool a = true, b = false
+
+	//Se a for verdadeiro
+	if (a){
+		printf("\n A eh verdadeiro")
+	}
+
+	//Comparando o B
+	if(b){
+		printf("\n b eh verdadeiro")
+
+	}else{
+		printf("\n b eh falso")
+	}
+	
+	// Comprando uma falsidade
+	if (!b){
+		printf("\n b eh falso")
+	}
+
+		system ("pause")
+
+}
