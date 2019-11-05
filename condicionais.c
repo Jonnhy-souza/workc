23fecb77c2d5dda09a47ee7e8dd9be758dd969bc
Jonnhy Souza <souzajonnhy@gmail.com>
2019-11-05 04:41:11 -0300

    Condicionais simples e compostas

---
+++ condicionais.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main
+
+
+//Definindo variáveis
+
+int a = 4, opcao = 1;
+float b = 2.5;
+char c = 'x';
+
+//Condicionais simples
+if (a == 5){
+	printf("\n A variavel a é = 5");
+}
+
+if (b == 2.5){
+	printf("\n A variavel b é = 2.5");
+}
+
+if (c == 'x'){
+	printf("\n A variavel c é = x");
+}
+
+// Numero par ou impar variavel composta simples
+
+if (a % 2 == 1){
+	printf("\n A variavel a é impar");
+
+}else{
+	printf("\n A variavel a é par");
+	
+
+// Variaveis composta
+
+if ( opcao == 1){
+	printf ("\n:w A opcao é = 1");	
+} else if { 
+	printf ("\n A opcao é = 2");
+}else{
+	printf ("\n Nenhuma das opcao está correta");
+}
+
+
+
+
+
+
+
