a6cb54075311f8f7f911c49927264c7058c13005
Jonnhy Souza <souzajonnhy@gmail.com>
2019-11-05 10:59:14 -0300

    condicionais char

---
+++ cond_char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdibo.h>
+
+void main(){
+
+	//Definindo variaveis
+	char letra = 'x';
+
+	//Condicional Simples
+	if (letra == 'x'){
+		printf ("\n a letra eh x.");
+	}
+		
+
+		printf("\nCodigo da letra = %d", letra);
+	if (letra == 120) {
+		printf("\n a letra eh x.");
+
+	}
+
+	system ("pause")
