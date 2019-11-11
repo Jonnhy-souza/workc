09b700d02e9e9754a4ba512b5a51b3307e29fd18
Jonnhy Souza <souzajonnhy@gmail.com>
2019-11-11 16:57:49 -0300

    laco while

---
+++ lacosrepeticao.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+
+	//variavel
+	int a = 1;
+	int b = 10;
+	//Contando até 10
+	while (a <= 10){
+	
+	//imprime a variavel
+		printf ("\n%d", a);
+
+	//Incremento
+	
+		a = a + 1; // a++;
+	}
+
+	//contagem regressiva
+	while (b >= 1){
+	a = a - 1; // b--;
+	system("pause");
+	} 
+
+
+}
+
+
