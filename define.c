50b14f63d27bcd6baaab5b46a44dbe3061d173e3
Jonnhy Souza <souzajonnhy@gmail.com>
2019-11-12 02:29:53 -0300

    definir uma váriavel constante

---
+++ define.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10 // define uma constante, constantes sempre escritas em maiusculas
+void main (){
+	
+
+
+	printf("%d", TAM);
+
+	
+	int i;
+
+
+	for (i = 1; i <= TAM;i++) {
+		printf("\n%d", i);
+
+
+}
